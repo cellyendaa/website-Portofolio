6accf95dba29f7cd34050d269ae34e36a147ae30
putricellyenda <putricellyenda16@gmail.com>
2025-03-04 22:55:19 +0700

    perubahan nama

--- src/About.js
+++ src/About.js
@@ -179,7 +179,7 @@ function AboutPage() {
             </div>
           </div>
         </div>
-        <div class=" flex justify-center items-center py-2">
+        <div class=" flex justify-center items-center py-2" id="View Resume">
           <div class="relative inline-flex  group">
             <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#e0c6f8] via-[#f1a9ea] to-[#f0b3b0] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
             <a
